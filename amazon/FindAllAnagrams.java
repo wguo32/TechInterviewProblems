e29a97ab7c0c7f3b569474aae7a214f96658307f
Shanfang <shanfangzhao@gmail.com>
2017-12-26 17:09:47 -0500

    anagrams problem solved with slide window

---
+++ amazon/FindAllAnagrams.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new LinkedList<>();
+        if (s == null || p == null || s.length() < p.length()) {
+            return result;
+        }
+
+        int[] hash = new int[256];
+        for (char c : p.toCharArray()) {
+            hash[c]++;
+        }
+        int needCount = p.length();
+        int start = 0, end = 0;
+        while (end < s.length()) {
+            if (hash[s.charAt(end)] >= 1) { // if it is 0, it means we don't need this character
+                needCount--;
+            }
+            hash[s.charAt(end)]--;
+            right++;
+
+            if (needCount == 0) {
+                result.add(start);
+            }
+            if (end - start == p.length()) {
+                if (hash[s.charAt(start)] >= 0) {
+                    needCount++;
+                }
+                hash[s.charAt(start)]++;
+                left++;
+            }
+        }
+        return result;
+    }
+}
