cfc4975512dad094fbccdbc6eab9f69e929af747
Shanfang <shanfangzhao@gmail.com>
2017-12-27 17:19:11 -0500

    simplify code for adding two numbers

--- amazon/AddTwoNumbers.java
+++ amazon/AddTwoNumbers.java
@@ -6,6 +6,45 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+
+ class Solution {
+     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+         int carry = 0;
+         ListNode dummy = new ListNode(-1);
+         ListNode curr = dummy;
+         while (l1 != null || l2 != null) {
+             // if (l1 == null) {
+             //     val1 = 0;
+             // } else {
+             //     val1 = l1.val;
+             // }
+             // if (l2 == null) {
+             //     val2 = 0;
+             // } else {
+             //     val2 = l2.val;
+             // }
+             int val1 = l1 == null ? 0 : l1.val;
+             int val2 = l2 == null ? 0 : l2.val;
+             int value = (val1 + val2 + carry) % 10;
+             carry = (val1 + val2 + carry) / 10;
+             ListNode newNode = new ListNode(value);
+             curr.next = newNode;
+             curr = newNode;
+             if (l1 != null ) {
+                 l1 = l1.next;
+             }
+             if (l2 != null) {
+                 l2 = l2.next;
+             }
+         }
+
+         if (carry != 0) {
+             curr.next = new ListNode(carry);
+         }
+         return dummy.next;
+     }
+ }
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int carry = 0;
