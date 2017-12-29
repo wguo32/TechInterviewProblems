bac6eb7a0cb1bd83e1a347a3fd3ca9f0df69ec1d
Shanfang <shanfangzhao@gmail.com>
2017-12-29 07:56:05 -0500

    rotate matrix by transpose then flip

---
+++ amazon/RotateImage.java
@@ -0,0 +1,32 @@
+/**
+Rotate the matrix clockwise by 90 degrees
+*/
+class Solution {
+    public void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return;
+        }
+
+        int len = matrix.length;
+        // transpose the matrix
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j < len; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // for each row of the matrix, flip it around the middle coloum
+        for (int i = 0; i < len; i++) {
+            int start = 0, end = len - 1;
+            while (start < end) {
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
