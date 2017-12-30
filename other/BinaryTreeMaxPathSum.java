3366feb29e38b55c3d37bf40ee036c5f644e89b7
Shanfang <shanfangzhao@gmail.com>
2017-12-30 09:11:13 -0500

    binary tree max path sum solved with divide and conquer

---
+++ other/BinaryTreeMaxPathSum.java
@@ -0,0 +1,50 @@
+/**
+http://www.lintcode.com/en/problem/binary-tree-maximum-path-sum/#
+
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+
+public class Solution {
+    /*
+     * @param root: The root of binary tree.
+     * @return: An integer
+     */
+
+    class Result {
+        int singlePath;
+        int maxPath;
+        Result(int single, int max) {
+            this.singlePath = single;
+            this.maxPath = max;
+        }
+    }
+    public int maxPathSum(TreeNode root) {
+        return pathHelper(root).maxPath;
+    }
+
+    public Result pathHelper(TreeNode root) {
+        if (root == null) {
+            return new Result(Integer.MIN_VALUE, Integer.MIN_VALUE);
+        }
+
+        Result leftResult = pathHelper(root.left);
+        Result rightResult = pathHelper(root.right);
+
+        int single = Math.max(Math.max(leftResult.singlePath, rightResult.singlePath), 0) + root.val;
+        int max = Math.max(leftResult.maxPath, rightResult.maxPath);
+        max = Math.max(
+                Math.max(leftResult.singlePath, 0) +
+                Math.max(rightResult.singlePath, 0) + root.val,
+                max);
+        return new Result(single, max);
+    }
+}
---
+++ other/BinaryTreeMaxPathSum2.java
@@ -0,0 +1,31 @@
+/**
+http://www.lintcode.com/en/problem/binary-tree-maximum-path-sum-ii/
+*/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+
+public class Solution {
+    /*
+     * @param root: the root of binary tree.
+     * @return: An integer
+     */
+    public int maxPathSum2(TreeNode root) {
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        }
+        int left = maxPathSum2(root.left);
+        int right = maxPathSum2(root.right);
+        return Math.max(0, Math.max(left, right)) + root.val;
+    }
+}
