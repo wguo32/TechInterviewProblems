361d540a511a1d8552b6c39adf98ad2c8f341c9f
Shanfang <shanfangzhao@gmail.com>
2017-12-30 10:53:19 -0500

    merge two binary trees iteratively with a stack

--- amazon/MergeTwoBinaryTree.java
+++ amazon/MergeTwoBinaryTree.java
@@ -25,3 +25,37 @@ class Solution {
 }
 
 // iterative approach
+class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        }
+        if (t2 == null) {
+            return t1;
+        }
+        Stack<TreeNode[]> stack = new Stack<>();
+        stack.push(new TreeNode[] {t1, t2});
+        while (!stack.isEmpty()) {
+            TreeNode[] pairs = stack.pop();
+            TreeNode node1 = pairs[0];
+            TreeNode node2 = pairs[1];
+            // only need to check the second node, because if the first node is null, it would not have been pushed onto stack
+            if (node2 == null) {
+                continue;
+            }
+            node1.val += node2.val;
+            if (node1.left == null) {
+                node1.left = node2.left;
+            } else {
+                stack.push(new TreeNode[] {node1.left, node2.left});
+            }
+
+            if (node1.right == null) {
+                node1.right = node2.right;
+            } else {
+                stack.push(new TreeNode[] {node1.right, node2.right});
+            }
+        }
+        return t1;
+    }
+}
