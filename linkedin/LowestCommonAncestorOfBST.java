08dbad9836c49f8b73fc4b27ee70f87aa8691bce
Shanfang <shanfangzhao@gmail.com>
2017-12-31 11:22:54 -0500

    rewrite LCA problem of BST taking advantage of BST property

--- linkedin/LowestCommonAncestorOfBST.java
+++ linkedin/LowestCommonAncestorOfBST.java
@@ -7,24 +7,19 @@
 *     TreeNode(int x) { val = x; }
 * }
 */
+
+// Take advantage of binary search tree property
 class Solution {
    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q) {
-            return root;
+        if (root == null) {
+            return null;
         }
-
-       TreeNode left = lowestCommonAncestor(root.left, p, q);
-       TreeNode right = lowestCommonAncestor(root.right, p, q);
-
-        if (left != null && right != null) {
-            return root;
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
         }
-       if (left != null) {
-           return left;
-       }
-       if (right != null) {
-           return right;
-       }
-       return null;
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
    }
 }
