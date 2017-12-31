1c6b61638ced8258c00e71d771d82751dffe4d39
Shanfang <shanfangzhao@gmail.com>
2017-12-31 18:18:11 -0500

    valid BST using divide and conquer

--- amazon/ValidBST.java
+++ amazon/ValidBST.java
@@ -30,3 +30,59 @@ class Solution {
         return true;
     }
 }
+
+
+/**
+using divide and conquer and Result class
+http://www.lintcode.com/en/problem/validate-binary-search-tree/
+*/
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+
+public class Solution {
+    /*
+     * @param root: The root of binary tree.
+     * @return: True if the binary tree is BST, or false
+     */
+    class Result {
+        int min;
+        int max;
+        boolean isBST;
+        Result(int minVal, int maxVal, boolean status) {
+            this.min = minVal;
+            this.max = maxVal;
+            this.isBST = status;
+        }
+    }
+    public boolean isValidBST(TreeNode root) {
+        return helper(root).isBST;
+    }
+    private Result helper(TreeNode root) {
+        if (root == null) {
+            return new Result(Integer.MAX_VALUE, Integer.MIN_VALUE, true);
+        }
+
+        Result leftResult = helper(root.left);
+        Result rightResult = helper(root.right);
+        if (!leftResult.isBST || !rightResult.isBST) {
+            return new Result(Integer.MIN_VALUE, Integer.MAX_VALUE, false);
+        }
+        if ((root.left != null && leftResult.max >= root.val) ||
+            (root.right != null && rightResult.min <= root.val)) {
+            return new Result(Integer.MIN_VALUE, Integer.MAX_VALUE, false);
+        }
+
+        return new Result(Math.min(root.val, leftResult.min),
+                            Math.max(root.val, rightResult.max), true);
+    }
+}
