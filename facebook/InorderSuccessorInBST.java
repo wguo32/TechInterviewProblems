ed13506cdfb4a6c2fa4f98be2815470574a322c3
Shanfang <shanfangzhao@gmail.com>
2018-01-01 20:29:36 -0500

    inorder successor in BST, recursive approach

---
+++ facebook/InorderSuccessorInBST.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ // recursive approach
+class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if (root == null || p == null) {
+            return null;
+        }
+
+        if (root.val <= p.val) {
+            return inorderSuccessor(root.right, p);
+        }
+        TreeNode leftSub = inorderSuccessor(root.left, p);
+        if (leftSub == null) {
+            return root;
+        }
+        return leftSub;
+    }
+}
