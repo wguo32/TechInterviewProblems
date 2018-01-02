3d447cb523556471644ba50815dd95597ce27796
Shanfang <shanfangzhao@gmail.com>
2018-01-01 20:16:47 -0500

    convert BST to larger tree in iterative approach using stack

--- amazon/ConvertBSTToLargerTree.java
+++ amazon/ConvertBSTToLargerTree.java
@@ -22,3 +22,27 @@ class Solution {
         return root;
     }
 }
+
+// approach2: using stack to do reverse inorder traversal
+class Solution {
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        int sum = 0;
+        while (!stack.isEmpty() || node != null) {
+            while (node != null) {
+                stack.push(node);
+                node = node.right;
+            }
+            node = stack.pop();
+            sum += node.val;
+            node.val = sum;
+            node = node.left;
+        }
+        return root;
+    }
+}
