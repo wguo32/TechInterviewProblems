1aa4eba73b724ae57525fd1432436f718be47faa
Shanfang <shanfangzhao@gmail.com>
2018-01-02 16:45:00 -0500

    optimized product without self such that no extra space is needed

--- amazon/ProductWithoutSelf.java
+++ amazon/ProductWithoutSelf.java
@@ -1,8 +1,5 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        if (nums == null) {
-            return new null;
-        }
         int len = nums.length;
         int[] leftP = new int[len];
         int[] rightP = new int[len];
@@ -22,5 +19,29 @@ class Solution {
             nums[i] = leftP[i] * rightP[i];
         }
         return nums;
+    }
+}
+
+// optimized with no extra space
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] result = new int[len];
+
+        int preProduct = 1;
+        for (int i = 0; i < len; i++) {
+            if (i > 0) {
+                preProduct *= nums[i - 1];
+            }
+            result[i] = preProduct;
+        }
+        int postProduct = 1;
+        for (int i = len - 1; i >= 0; i--) {
+            if (i < len - 1) {
+                postProduct *= nums[i + 1];
+            }
+            result[i] *= postProduct;
+        }
+        return result;
     }
 }
