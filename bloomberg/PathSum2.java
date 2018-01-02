fdbcb8ab5179b19edbc7534e8b4abc22e4dfafd2
Shanfang <shanfangzhao@gmail.com>
2018-01-02 10:27:01 -0500

    pathsum2 solved with backtracking

---
+++ bloomberg/PathSum2.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+ // use backtracking, similar to subsets problem
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        helper(result, new ArrayList<>(), root, sum);
+        return result;
+    }
+
+    private void helper(List<List<Integer>> result, List<Integer> list, TreeNode root, int sum) {
+        if (root == null) {
+            return;
+        }
+        list.add(root.val);
+        if (root.left == null && root.right == null && root.val == sum) {
+            result.add(new ArrayList<>(list));
+        } else {
+            helper(result, list, root.left, sum - root.val);
+            helper(result, list, root.right, sum - root.val);
+        }
+        list.remove(list.size() - 1);
+    }
+}
