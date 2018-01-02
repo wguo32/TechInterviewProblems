75f3e5b9fa91ad280228bfa79dde41c83943dbf1
Shanfang <shanfangzhao@gmail.com>
2018-01-02 08:45:18 -0500

    binary tree path sum, recursive approach

---
+++ other/PathSum3.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        return pathSum(root.left, sum) + pathSum(root.right, sum) +
+            startFromRoot(root, sum);
+    }
+
+    private int startFromRoot(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        return (root.val == sum ? 1 : 0) + startFromRoot(root.left, sum - root.val)
+                            + startFromRoot(root.right, sum - root.val);
+    }
+}
