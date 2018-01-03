28425105c3eb74ed13d4270c6c6430b9cbdabfc5
Shanfang <shanfangzhao@gmail.com>
2018-01-03 11:24:11 -0500

    fix bug in amazon OA-the maze

--- amazon/TheMaze.java
+++ amazon/TheMaze.java
@@ -6,16 +6,8 @@
 import java.util.LinkedList;
 import java.util.Queue;
 public class TheMaze {
-    int[] deltaX = {-1, 0, 0 ,1};
-    int[] deltaY = {0, 1, -1, 0};
-    class Point {
-        int x;
-        int y;
-        Point(int a, int b) {
-            this.x = a;
-            this.y = b;
-        }
-    }
+    int[][] dir = {{-1, 0}, {0, 1}, {0, -1}, {1, 0}};
+
     public int solution(int[][] maze) {
         if (maze == null || maze.length == 0 || maze[0].length == 0) {
             return 0;
@@ -24,42 +16,47 @@ public class TheMaze {
             return 0;
         }
 
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(0, 0));
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, 0});
+        maze[0][0] = 0;
         while (!queue.isEmpty()) {
-            Point point = queue.poll();
-            if (maze[point.x][point.y] == 9) {
-                return 1;
-            } else if (maze[point.x][point.y] == 1) {
-                for (int i = 0; i < 4; i++) {
-                    int newX = point.x + deltaX[i];
-                    int newY = point.y + deltaY[i];
-                    if (newX >= 0 && newX < maze.length && newY >= 0 && newY < maze[0].length) {
-                        queue.offer(new Point(newX, newY));
+            int[] point = queue.poll();
+            System.out.println("In the while loop");
+            for (int i = 0; i < 4; i++) {
+                int newX = point[0] + dir[i][0];
+                int newY = point[1] + dir[i][1];
+
+                if (newX >= 0 && newX < maze.length && newY >= 0 && newY < maze[0].length) {
+                    System.out.println("In the for loop");
+
+                    if (maze[newX][newY] == 9) {
+                        return 1;
+                    } else if (maze[newX][newY] == 1) {
+                        queue.offer(new int[] {newX, newY});
+                        maze[newX][newY] = 0;
+                        System.out.println("Current position:" + newX+ ", " + newY);
                     }
                 }
-                System.out.println("Current position:" + point.x + ", " + point.y);
-                maze[point.x][point.y] = 0;
             }
         }
         return 0;
     }
 
     public static void main(String[] args) {
-        // int[][] matrix = {
-        //     {1,1,0,1,1},
-        //     {1,1,1,1,1},
-        //     {1,0,1,1,1},
-        //     {0,0,1,0,0},
-        //     {9,1,1,1,1}
-        // };
         int[][] matrix = {
             {1,1,0,1,1},
             {1,1,1,1,1},
             {1,0,1,1,1},
             {0,0,1,0,0},
-            {1,1,0,1,9}
+            {9,1,1,1,1}
         };
+        // int[][] matrix = {
+        //     {1,1,0,1,1},
+        //     {1,1,1,1,1},
+        //     {1,0,1,1,1},
+        //     {0,0,1,0,0},
+        //     {1,1,0,1,9}
+        // };
         TheMaze maze = new TheMaze();
         System.out.println(maze.solution(matrix));
     }
