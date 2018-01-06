7fb96fcd7fbc8444c47ca3efe25e4b41a2ffe879
Shanfang <shanfangzhao@gmail.com>
2018-01-06 04:47:52 -0500

    iterative approach to solve letter combination of phone number problem

--- amazon/LetterCombinationPhoneNumber.java
+++ amazon/LetterCombinationPhoneNumber.java
@@ -26,3 +26,26 @@ class Solution {
         }
     }
 }
+
+
+// iterative approach
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if (digits.isEmpty()) {
+            return new ArrayList<>();
+        }
+        LinkedList<String> result = new LinkedList<>();
+        result.offer("");
+        String[] mapping = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        while (!result.isEmpty()) {
+            if (result.peek().length() == digits.length()) {
+                break;
+            }
+            String str = result.poll();
+            for (char ch : mapping[digits.charAt(str.length()) - '0'].toCharArray()) {
+                result.offer(str + ch);
+            }
+        }
+        return result;
+    }
+}
