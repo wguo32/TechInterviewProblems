ea263fcaa099490ee9c739b1282e2b6dd0fabe88
Shanfang <shanfangzhao@gmail.com>
2018-01-08 17:43:44 -0500

    plus one

---
+++ google/PlusOne.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1; // other digits are 0s
+        return newDigits;
+    }
+}
