e9da5944c0c0268f3dbd90d6fe688c454d7c2458
Shanfang <shanfangzhao@gmail.com>
2018-01-09 17:51:44 -0500

    subarray with max sum

---
+++ linkedin/MaxSubarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int maxCur = nums[0], maxSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            maxCur = Math.max(maxCur + nums[i], nums[i]);
+            maxSum = Math.max(maxSum, maxCur);
+        }
+        return maxSum;
+    }
+}
