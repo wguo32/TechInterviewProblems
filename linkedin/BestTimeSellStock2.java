60b1072f2d2b9aaf17f0d84de08e746b580d3bad
Shanfang <shanfangzhao@gmail.com>
2018-01-09 21:21:35 -0500

    best time to sell stocks, multiple transactions

---
+++ linkedin/BestTimeSellStock2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                maxProfit += prices[i] - prices[i - 1];
+            }
+        }
+        return maxProfit;
+    }
+}
