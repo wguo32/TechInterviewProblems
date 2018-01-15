9bf5e835d9cbdec0b247562fbb3929486ec0a9e0
Shanfang <shanfangzhao@gmail.com>
2018-01-15 09:59:08 -0500

    solve find disappear number problem again

--- google/FindDisappearedNumbers.java
+++ google/FindDisappearedNumbers.java
@@ -1,17 +1,18 @@
-public List<Integer> findDisappearedNumbers(int[] nums) {
-    List<Integer> ret = new ArrayList<Integer>();
-
-    for(int i = 0; i < nums.length; i++) {
-        int val = Math.abs(nums[i]) - 1;
-        if(nums[val] > 0) {
-            nums[val] = -nums[val];
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = Math.abs(nums[i]);
+            if (nums[num - 1] > 0) {
+                nums[num - 1] = -nums[num - 1];
+            }
         }
-    }
 
-    for(int i = 0; i < nums.length; i++) {
-        if(nums[i] > 0) {
-            ret.add(i+1);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
         }
+        return result;
     }
-    return ret;
 }
