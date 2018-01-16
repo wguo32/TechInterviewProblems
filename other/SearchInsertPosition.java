0e9bfb5f92bad253387200896ca5859e7389e4e9
Shanfang <shanfangzhao@gmail.com>
2018-01-16 12:21:03 -0500

    find or insert using binary search

---
+++ other/SearchInsertPosition.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = (end - start) / 2 + start;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (nums[start] >= target) {
+            return start;
+        } else if (nums[end] >= target) {
+            return end;
+        } else {
+            return end + 1;
+        }
+    }
+}
