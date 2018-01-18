b48cb80fadf64662246d3f681aa0f9d0dcdab8d9
Shanfang <shanfangzhao@gmail.com>
2018-01-18 16:28:13 -0500

    convert roman to integer without hashmap

--- microsoft/RomanToInteger.java
+++ microsoft/RomanToInteger.java
@@ -1,8 +1,10 @@
+// use HashMap
 class Solution {
     public int romanToInt(String s) {
         if (s == null || s.length() == 0) {
             return 0;
         }
+
         Map<Character, Integer> map = new HashMap<>();
         map.put('I', 1);
         map.put('V', 5);
@@ -12,20 +14,49 @@ class Solution {
         map.put('D', 500);
         map.put('M', 1000);
 
+        char[] chars = s.toCharArray();
         int sum = 0;
-        int pre = map.get(s.charAt(0));
-        for (int i = 1; i < s.length(); i++) {
-            int next = map.get(s.charAt(i));
-            if (pre < next) {
+        int pre = map.get(chars[0]);
+        for (int i = 1; i < chars.length; i++) {
+            if (map.get(chars[i]) > pre) {
                 sum -= pre;
-            }
-            else {
+            } else {
                 sum += pre;
             }
-            pre = next;
+            pre = map.get(chars[i]);
         }
-
         sum += pre;
         return sum;
+    }
+}
+
+// no HashMap
+class Solution {
+    public int romanToInt(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        char[] chars = s.toCharArray();
+        int sum = convert(chars[0]);
+        for (int i = 1; i < chars.length; i++) {
+            sum += convert(chars[i]);
+            if (convert(chars[i - 1]) < convert(chars[i])) {
+                sum -= 2 * convert(chars[i - 1]);
+            }
+        }
+        return sum;
+    }
+    private int convert(char c) {
+        switch (c) {
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+        }
+        return 0;
     }
 }
