d39a352cd7d73469cadbc4ae29b1d650b28aad99
Shanfang <shanfangzhao@gmail.com>
2018-01-18 17:45:19 -0500

    unique paths solved with better space complexity

--- bloomberg/UniquePaths.java
+++ bloomberg/UniquePaths.java
@@ -20,3 +20,24 @@ class Solution {
         return dp[m - 1][n - 1];
     }
 }
+
+// better space complexity
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if (m <= 0 || n <= 0) {
+            return 0;
+        }
+        int[][] memo = new int[2][n];
+        for (int i = 0; i < n; i++) {
+            memo[0][i] = 1;
+        }
+        memo[1][0] = 1;
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                memo[i % 2][j] = memo[i % 2][j - 1] + memo[(i - 1) % 2][j];
+            }
+        }
+        return memo[(m - 1) % 2][n - 1];
+    }
+}
