bbe2915773a7494fca70b854394ed891e3b381ee
Shanfang <shanfangzhao@gmail.com>
2018-01-18 17:09:27 -0500

    triangle problem solved without extra space using DP

--- other/Triangle.java
+++ other/Triangle.java
@@ -1,31 +1,22 @@
+// bottom up approach
 class Solution {
-    // public int minimumTotal(List<List<Integer>> triangle) {
-    //     if (triangle == null || triangle.size() == 0) {
-    //         return Integer.MAX_VALUE;
-    //     }
-    //     int minTotal = Integer.MAX_VALUE;
-    //     return helper(triangle, 0, 0);
-    // }
-    //
-    // private int helper(List<List<Integer>> triangle, int level, int index) {
-    //     if (level == triangle.size()) {
-    //         return 0;
-    //     }
-    //
-    //     List<Integer> list = triangle.get(level);
-    //     int min = Integer.MAX_VALUE;
-    //     return list.get(index) +
-    //             Math.min(helper(triangle, level + 1, index),
-    //                     helper(triangle, level + 1, index + 1));
-    // }
-
     public int minimumTotal(List<List<Integer>> triangle) {
-        int[] A = new int[triangle.size()+1];
-        for(int i=triangle.size()-1;i>=0;i--){
-            for(int j=0;j<triangle.get(i).size();j++){
-                A[j] = Math.min(A[j],A[j+1])+triangle.get(i).get(j);
+        if (triangle == null || triangle.size() == 0) {
+            return 0;
+        }
+
+        int len = triangle.size();
+        int[] arr = new int[len];
+        int lastRow = triangle.get(len - 1);
+        for (int i = 0; i < len; i++) {
+            arr[i] = lastRow.get(i);
+        }
+        for (int i = triangle.size() - 2; i >= 0; i--) {
+            List<Integer> list = triangle.get(i);
+            for (int j = 0; j <= i; j++) {
+                arr[j] = Math.min(arr[j], arr[j + 1]) + list.get(j);
             }
         }
-        return A[0];
+        return arr[0];
     }
 }
