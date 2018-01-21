cfb0c2722d280d0550b3b5f3fd06b18b2f4c455b
Shanfang <shanfangzhao@gmail.com>
2018-01-21 17:41:45 -0500

    find intersection of two arrays using two pointers

--- other/IntersectionTwoArrays.java
+++ other/IntersectionTwoArrays.java
@@ -1,3 +1,37 @@
+// two pointers
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null ||
+            nums1.length == 0 || nums2.length == 0) {
+            return new int[0];
+        }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0, j = 0;
+
+        Set<Integer> intersect = new HashSet<>();
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                intersect.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+
+        int[] result = new int[intersect.size()];
+        int index = 0;
+        for (int num : intersect) {
+            result[index++] = num;
+        }
+        return result;
+    }
+}
+
+// use two hashset
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         if (nums1 == null || nums2 == null ||
