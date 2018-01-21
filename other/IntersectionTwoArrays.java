0a493a073e7d710dce51340706d415bb0a892ab2
Shanfang <shanfangzhao@gmail.com>
2018-01-21 17:54:28 -0500

    find intersection of two arrays using binary search

--- other/IntersectionTwoArrays.java
+++ other/IntersectionTwoArrays.java
@@ -58,3 +58,44 @@ class Solution {
         return intersect;
     }
 }
+
+// binary search
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null ||
+            nums1.length == 0 || nums2.length == 0) {
+            return new int[0];
+        }
+        Arrays.sort(nums1);
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums2) {
+            if (search(nums1, num)) {
+                set.add(num);
+            }
+        }
+        int[] result = new int[set.size()];
+        int index = 0;
+        for (int num : set) {
+            result[index++] = num;
+        }
+        return result;
+    }
+
+    private boolean search(int[] nums, int num) {
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = (end - start) / 2 + start;
+            if (nums[mid] > num) {
+                end = mid;
+            } else if (nums[mid] < num){
+                start = mid;
+            } else {
+                return true;
+            }
+        }
+        if (nums[start] == num || nums[end] == num) {
+            return true;
+        }
+        return false;
+    }
+}
