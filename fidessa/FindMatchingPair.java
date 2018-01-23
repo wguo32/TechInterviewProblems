f9b4f2360e54b60834a1736f5cb5f2dc53ad7725
Shanfang <shanfangzhao@gmail.com>
2018-01-23 15:52:08 -0500

    find matching pair in string wity Deque

---
+++ fidessa/FindMatchingPair.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class FindMatchingPair {
+    public static int match(String input) {
+        if (input == null || input.length() == 0) {
+            return -1;
+        }
+        int result = -1;
+        Deque<Character> dq = new LinkedList<>();
+        char[] chars = input.toCharArray();
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isLetter(chars[i])) {
+                break;
+            }
+            if (Character.isUpperCase(chars[i])) {
+                dq.add(chars[i]);
+            } else {
+                if (dq.isEmpty()) {
+                    break;
+                } else {
+                    if (dq.peekLast() == Character.toUpperCase(chars[i])) {
+                        result = i;
+                        dq.removeLast();
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+        System.out.println(match(str));
+    }
+}
