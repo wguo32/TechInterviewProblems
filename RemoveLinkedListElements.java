f2621430470a5991d08ea70783e6491da4d3ab0b
Shanfang <shanfangzhao@gmail.com>
2018-01-23 21:58:34 -0500

    remove nodes with given value from linked list, iterative approach

---
+++ RemoveLinkedListElements.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ // iterative approach
+ class Solution {
+     public ListNode removeElements(ListNode head, int val) {
+         ListNode dummy = new ListNode(0);
+         dummy.next = head;
+         head = dummy;
+
+         while (head.next != null) {
+             if (head.next.val == val) {
+                 head.next = head.next.next;
+             } else {
+                 head = head.next;
+             }
+         }
+         return dummy.next;
+     }
+ }
