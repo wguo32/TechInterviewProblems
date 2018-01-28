9961025e2cf1ac91176fe00cd6c097e68cdbb4e2
Shanfang <shanfangzhao@gmail.com>
2018-01-27 21:17:31 -0500

    reverse a linked list in iterative approach

---
+++ microsoft/ReverseLinkedList.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ // iterative approach
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+}
