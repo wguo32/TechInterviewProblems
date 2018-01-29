a82b0ef492b04f6907091d5afb0ce02966f997a5
Shanfang <shanfangzhao@gmail.com>
2018-01-28 21:43:09 -0500

    happy number

---
+++ twitter/HappyNumber.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> hash = new HashSet<Integer>();
+        while (n != 1) {
+            if (hash.contains(n)) {
+                return false;
+            }
+            hash.add(n);
+            n = getNextHappy(n);
+        }
+        return true;
+    }
+    private int getNextHappy(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return sum;
+    }
+}
