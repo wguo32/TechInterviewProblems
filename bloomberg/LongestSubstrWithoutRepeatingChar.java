f7d5a97819dcc93680db2c31d151f00dec5fc046
Shanfang <shanfangzhao@gmail.com>
2018-01-31 20:21:41 -0500

    find longest substring without repeating character using two pointers

---
+++ bloomberg/LongestSubstrWithoutRepeatingChar.java
@@ -0,0 +1,29 @@
+Given a string, find the length of the longest substring without repeating characters.
+
+Examples:
+
+Given "abcabcbb", the answer is "abc", which the length is 3.
+
+Given "bbbbb", the answer is "b", with the length of 1.
+
+Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        int start = 0, end = 0;
+        int maxLen = 0;
+        while (end < s.length()) {
+            if (map.containsKey(s.charAt(end))) {
+                start = Math.max(start, map.get(s.charAt(end)) + 1);
+            }
+            map.put(s.charAt(end), end);
+            maxLen = Math.max(maxLen, end - start + 1);
+            end++;
+        }
+        return maxLen;
+    }
+}
