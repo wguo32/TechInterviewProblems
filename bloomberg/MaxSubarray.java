99f8a6c517278e664417f4179cd2deaf151772ce
Shanfang <shanfangzhao@gmail.com>
2018-02-01 10:37:19 -0500

    max subarray solved with DP

---
+++ bloomberg/MaxSubarray.java
@@ -0,0 +1,17 @@
+// use DP
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+            maxSum = Math.max(maxSum, dp[i]);
+        }
+        return maxSum;
+    }
+}
