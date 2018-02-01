de544da40786952b5209ce6b461669fb2840852b
Shanfang <shanfangzhao@gmail.com>
2018-02-01 15:47:17 -0500

    copy list with random pointers with constant space

--- bloomberg/CopyListWithRandomPointers.java
+++ bloomberg/CopyListWithRandomPointers.java
@@ -6,6 +6,49 @@
  *     RandomListNode(int x) { this.label = x; }
  * };
  */
+// constant space complexity
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        RandomListNode cur = head;
+        // duplicate each node in the original RandomListNode
+        while (cur != null) {
+            RandomListNode temp = cur.next;
+            RandomListNode newNode  = new RandomListNode(cur.label);
+            cur.next = newNode;
+            newNode.next = temp;
+            cur = temp;
+        }
+
+        // copy random pointers
+        cur = head;
+        while (cur != null) {
+            if (cur.random != null) {
+               cur.next.random = cur.random.next;
+            }
+            cur = cur.next.next;
+        }
+
+        // restore the original list
+        cur = head;
+        RandomListNode dummy = new RandomListNode(0);
+        RandomListNode newCur, newIter = dummy;
+        while (cur != null) {
+            RandomListNode temp = cur.next.next;
+            newCur = cur.next;
+            newIter.next = newCur;
+            newIter = newCur;
+
+            cur.next = temp;
+            cur = temp;
+        }
+        return dummy.next;
+    }
+}
+
 
  // O(N) space complexity
 public class Solution {
