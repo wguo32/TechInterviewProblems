36c9f0f2ec937e9f7ee0345f980c6935210b8fe3
Shanfang <shanfangzhao@gmail.com>
2018-01-31 19:31:39 -0500

    container with most water solved with two pointers

--- bloomberg/ContainerWithMostRain.java
+++ bloomberg/ContainerWithMostRain.java
@@ -1,14 +1,16 @@
+Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+Note: You may not slant the container and n is at least 2.
 class Solution {
     public int maxArea(int[] height) {
-        if (height == null || height.length <= 1) {
-            return 0;
-        }
-        int maxArea = 0;
         int left = 0, right = height.length - 1;
+        int maxArea = 0;
         while (left < right) {
-            int newArea = (right - left) * Math.min(height[right], height[left]);
-            maxArea = Math.max(maxArea, newArea);
-            if (height[right] <= height[left]) {
+            int area = (right - left) * Math.min(height[right], height[left]);
+            if (area > maxArea) {
+                maxArea = area;
+            }
+            if (height[right] < height[left]) {
                 right--;
             } else {
                 left++;
