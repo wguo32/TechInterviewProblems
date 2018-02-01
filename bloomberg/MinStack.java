cbe8493e0e380c7a21c031361f5c42955f7f06fb
Shanfang <shanfangzhao@gmail.com>
2018-02-01 09:38:59 -0500

    implement min stack with one stack

--- bloomberg/MinStack.java
+++ bloomberg/MinStack.java
@@ -1,3 +1,4 @@
+// use two stacks
 class MinStack {
 
         Stack<Integer> stack1;
@@ -32,6 +33,39 @@ class MinStack {
     }
 }
 
+
+class MinStack {
+    Stack<Integer> stack;
+    int min;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        min = Integer.MAX_VALUE;
+    }
+
+    public void push(int x) {
+        if (x <= min) {
+            stack.push(min);
+            min = x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        if (stack.pop() == min) {
+            min = stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
