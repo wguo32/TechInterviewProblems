500b568e1d08e57a6d04a74fd0a4e6a9941d1d5c
Shanfang <shanfangzhao@gmail.com>
2018-02-03 20:23:41 -0500

    rotate array using reverse three times

---
+++ bloomberg/RotateArray.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if (nums == null || k <= 0) {
+            return;
+        }
+
+        int step = k % nums.length;
+        reverse(nums, 0, nums.length - step - 1);
+        reverse(nums, nums.length - step, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+    }
+
+    public void reverse(int[] array, int start, int end) {
+        while (start < end) {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
