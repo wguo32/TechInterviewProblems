7acf20e61f6e1b73255eec5c0189e8e8c40c7b26
Shanfang <shanfangzhao@gmail.com>
2018-02-05 18:57:20 -0500

    Kth largest element solved with quick select

--- bloomberg/KthLargestElement.java
+++ bloomberg/KthLargestElement.java
@@ -1,4 +1,4 @@
-// using priority queue
+// using priority queue, time complexity O(nlogn), space complexity is O(k)
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         Queue<Integer> queue = new PriorityQueue<>();
@@ -9,5 +9,53 @@ class Solution {
             }
         }
         return queue.peek();
+    }
+}
+
+// quick select, time complexity worse case O(N^2) and best case O(N)
+// without constant extra space
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int left = 0, right = nums.length - 1;
+        while (true) {
+            int pos = partition(left, right, nums);
+            if (pos + 1 == k) {
+                return nums[pos];
+            } else if (pos + 1 > k) {
+                right = pos - 1;
+            } else {
+                left = pos + 1;
+            }
+        }
+    }
+    private int partition(int left, int right, int[] nums) {
+        int pivot = nums[left];
+        int start = left + 1;
+
+        while (start <= right) {
+            if (nums[start] < pivot && nums[right] > pivot) {
+                swap(nums, start, right);
+                start++;
+                right--;
+            }
+            if (nums[start] >= pivot) {
+                start++;
+            }
+            if (nums[right] <= pivot) {
+                right--;
+            }
+        }
+        swap(nums, left, right);
+        return right;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
