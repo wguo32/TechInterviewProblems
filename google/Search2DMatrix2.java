e4d464d33f7c716f0664f67e5c1300022e7aac26
Shanfang <shanfangzhao@gmail.com>
2018-02-05 16:27:47 -0500

    search 2D matrix II with tricks

---
+++ google/Search2DMatrix2.java
@@ -0,0 +1,24 @@
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+
+// time complexity O (M + N), where M is #row and N is #col
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int row = 0, col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+}
