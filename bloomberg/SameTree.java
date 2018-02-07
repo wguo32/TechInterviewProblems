7f933c1017bd9c7196fef6dcaf449309582dd47b
Shanfang <shanfangzhao@gmail.com>
2018-02-07 11:42:33 -0500

    recursively check if two binary trees are the same

---
+++ bloomberg/SameTree.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ // iterative approach
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && isSameTree(p.left, q.left) &&
+                isSameTree(p.right, q.right);
+    }
+}
