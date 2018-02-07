a3c6377e85a9a468b6fabd790036b25dd58caa27
Shanfang <shanfangzhao@gmail.com>
2018-02-07 13:39:10 -0500

    iteratively determine if two binary trees are the same using two stacks

--- bloomberg/SameTree.java
+++ bloomberg/SameTree.java
@@ -7,7 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
- // iterative approach
+ // recursive approach
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
@@ -18,5 +18,47 @@ class Solution {
         }
         return p.val == q.val && isSameTree(p.left, q.left) &&
                 isSameTree(p.right, q.right);
+    }
+}
+
+// iterative approach
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        stack1.push(p);
+        stack2.push(q);
+        while (!stack1.isEmpty() && !stack2.isEmpty()) {
+            p = stack1.pop();
+            q = stack2.pop();
+            if (p.val != q.val) {
+                return false;
+            }
+            if (p.left != null) {
+                stack1.push(p.left);
+            }
+            if (q.left != null) {
+                stack2.push(q.left);
+            }
+            if (stack1.size() != stack2.size()) {
+                return false;
+            }
+            if (p.right != null) {
+                stack1.push(p.right);
+            }
+            if (q.right != null) {
+                stack2.push(q.right);
+            }
+            if (stack1.size() != stack2.size()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
