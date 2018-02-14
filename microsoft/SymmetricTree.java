d49de2301aa8d96c51e0d2f776352440b7f51906
Shanfang <shanfangzhao@gmail.com>
2018-02-13 20:46:54 -0500

    iterative approach to check if a binary tree is symmetric

--- microsoft/SymmetricTree.java
+++ microsoft/SymmetricTree.java
@@ -7,6 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ // recursive approach
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         if (root == null) {
@@ -23,5 +24,32 @@ class Solution {
             return false;
         }
         return n1.val == n2.val && helper(n1.left, n2.right) && helper(n1.right, n2.left);
+    }
+}
+
+// iterative approach
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode p = queue.poll();
+            TreeNode q = queue.poll();
+            if (p == null && q == null) {
+                continue;
+            }
+            if (p == null || q == null) {
+                return false;
+            }
+            if (p.val != q.val) {
+                return false;
+            }
+            queue.offer(p.left);
+            queue.offer(q.right);
+            queue.offer(p.right);
+            queue.offer(q.left);
+        }
+        return true;
     }
 }
