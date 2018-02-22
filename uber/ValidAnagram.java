434ee19e1268c32c9e972d46c7ed89b92789c98b
Shanfang <shanfangzhao@gmail.com>
2018-02-21 22:20:50 -0500

    valid anagrams use sorting

---
+++ uber/ValidAnagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] charS = s.toCharArray();
+        char[] charT = t.toCharArray();
+        Arrays.sort(charS);
+        Arrays.sort(charT);
+        for (int i = 0; i < s.length(); i++) {
+            if (charS[i] != charT[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
