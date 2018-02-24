9a081152e47cb08d91099e74b4f39ff9d5c2ff2b
Shanfang <shanfangzhao@gmail.com>
2018-02-23 21:53:12 -0500

    fix edge case bug

--- adobe/ClimbingStairs.java
+++ adobe/ClimbingStairs.java
@@ -1,5 +1,8 @@
 class Solution {
     public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
         int[] table = new int[n];
         table[0] = 1;
         table[1] = 2;
@@ -7,5 +10,19 @@ class Solution {
             table[i] = table[i - 1] + table[i - 2];
         }
         return table[n - 1];
+    }
+}
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 }
