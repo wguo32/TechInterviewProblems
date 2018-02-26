aa7abbb4075dde48556ae3acc874f47e27df421d
Shanfang <shanfangzhao@gmail.com>
2018-02-26 16:45:55 -0500

    add digits in non-recursive approach

--- microsoft/AddDigits.java
+++ microsoft/AddDigits.java
@@ -13,3 +13,19 @@ class Solution {
         }
     }
 }
+
+
+// iterative approach
+class Solution {
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int result = 0;
+            while (num != 0) {
+                result += num % 10;
+                num = num / 10;
+            }
+            num = result;
+        }
+        return num;
+    }
+}
