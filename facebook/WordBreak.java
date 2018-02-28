56c926683ac5fe4c462273ab41be6f1dbdde284f
Shanfang <shanfangzhao@gmail.com>
2018-02-27 20:07:32 -0500

    word break solve with DP

--- facebook/WordBreak.java
+++ facebook/WordBreak.java
@@ -1,17 +1,13 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> set = new HashSet<>();
-        for (String word : wordDict) {
-            set.add(word);
-        }
-
         int len = s.length();
         boolean[] breakable = new boolean[len + 1];
         breakable[0] = true;
+
         for (int i = 1; i <= len; i++) {
             for (int j = 0; j < i; j++) {
                 String substr = s.substring(j, i);
-                if (set.contains(substr) && breakable[j]) {
+                if (breakable[j] && wordDict.contains(substr)) {
                     breakable[i] = true;
                     break;
                 }
