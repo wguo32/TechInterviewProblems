85971030181cb8ed13bd618dad16655f6f45e6f9
Shanfang <shanfangzhao@gmail.com>
2018-03-02 15:45:56 -0500

    valid parentheses

---
+++ fanatics/ValidParentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else {
+                if (stack.isEmpty() || c != stack.pop()) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
