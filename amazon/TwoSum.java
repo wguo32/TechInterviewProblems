ef3ee7e71c3d667425750f2aeb98833882281e83
Shanfang <shanfangzhao@gmail.com>
2018-03-05 14:35:50 -0500

    two sum2 with array sorted

--- amazon/TwoSum.java
+++ amazon/TwoSum.java
@@ -1,11 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> hash = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                return new int[]{map.get(nums[i]), i};
+            if (hash.containsKey(nums[i])) {
+                return new int[]{hash.get(nums[i]), i};
+            } else {
+                hash.put(target - nums[i], i);
             }
-            map.put(target - nums[i], i);
         }
         return new int[]{};
     }
---
+++ amazon/TwoSum2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int i = 0, j = numbers.length - 1;
+        while (i < j) {
+            if (numbers[i] + numbers[j] == target) {
+                return new int[]{i + 1, j + 1};
+            } else if (numbers[i] + numbers[j] < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return new int[]{};
+    }
+}
