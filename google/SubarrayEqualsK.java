34771f090cc07a7bf2a7471568c1cc5955eeba86
Shanfang <shanfangzhao@gmail.com>
2018-03-05 16:31:17 -0500

    subarray sum equals K with no extra space

--- google/SubarrayEqualsK.java
+++ google/SubarrayEqualsK.java
@@ -21,3 +21,23 @@ class Solution {
         return count;
     }
 }
+
+// no extra space needed
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
