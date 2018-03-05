4b750b74d4b1ff7031bb2bb7dbf27d5c55aa5e9e
Shanfang <shanfangzhao@gmail.com>
2018-03-05 16:49:36 -0500

    subarray sum equals k with linear time complexity

--- google/SubarrayEqualsK.java
+++ google/SubarrayEqualsK.java
@@ -41,3 +41,25 @@ class Solution {
         return count;
     }
 }
+
+// linear time using hashmap
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        map.put(0, 1);
+        int count = 0;
+
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
