f613e282f3c8c1a200577b509861654f87a7e39c
Shanfang <shanfangzhao@gmail.com>
2018-03-06 21:05:51 -0500

    max subarray solved with DP

---
+++ microsoft/MaxSubarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int len = nums.length;
+        int[] dp = new int[len];
+        dp[0] = nums[0];
+        int max = dp[0];
+        for (int i = 1; i < len; i++) {
+            if (dp[i - 1] >= 0) {
+                dp[i] = dp[i - 1] + nums[i];
+            } else{
+                dp[i] = nums[i];
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
