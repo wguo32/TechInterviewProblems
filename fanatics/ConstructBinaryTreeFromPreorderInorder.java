e54b6966555f510bcfcaa68abbbffb19085b8281
Shanfang <shanfangzhao@gmail.com>
2018-03-09 10:58:38 -0500

    construct binary tree from inorder and preorder traversal

---
+++ fanatics/ConstructBinaryTreeFromPreorderInorder.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0 ||
+            inorder == null || inorder.length == 0) {
+                return null;
+        }
+        return helper(preorder, inorder, 0, inorder.length - 1, 0);
+    }
+
+    private TreeNode helper(int[] preorder, int[] inorder, int inStart, int inEnd, int preStart) {
+        if (inStart > inEnd || preStart >= preorder.length) {
+            return null;
+        }
+        int index = getIndex(inorder, preorder[preStart]);
+        int leftCount = index - inStart + 1;
+        int preStartRight = preStart + leftCount;
+        TreeNode root = new TreeNode(inorder[index]);
+        root.left = helper(preorder, inorder, inStart, index - 1);
+        root.right = helper(preorder, inorder, index + 1, inEnd, preStartRight);
+        return root;
+    }
+
+    private int getIndex(int[] nums, int num) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == num) {
+                return i;
+            }
+        }
+    }
+}
