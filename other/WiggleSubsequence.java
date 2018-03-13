28d2b0f4dcd5bdc3ce6fc8f9b7d57a6b7a289271
Shanfang <shanfangzhao@gmail.com>
2018-03-13 16:45:33 -0400

    longest wiggle subsequence solved with DP in constant space complexity

--- other/WiggleSubsequence.java
+++ other/WiggleSubsequence.java
@@ -24,3 +24,21 @@ class Solution {
         return Math.max(up[nums.length - 1], down[nums.length - 1]);
     }
 }
+
+// optimized with constant space
+class Solution {
+    public int wiggleMaxLength(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int up = 1, down = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up = down + 1;
+            } else if (nums[i] < nums[i - 1]) {
+                down = up + 1;
+            }
+        }
+        return Math.max(up, down);
+    }
+}
