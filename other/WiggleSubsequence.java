7b737a13b1feecf13b1335ad5895e48a7b19411f
Shanfang <shanfangzhao@gmail.com>
2018-03-13 16:31:22 -0400

    longest wiggle subsequence solved with DP

---
+++ other/WiggleSubsequence.java
@@ -0,0 +1,26 @@
+// DP time complexity O(N), space complexity O(N)
+class Solution {
+    public int wiggleMaxLength(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] down = new int[nums.length];
+        int[] up = new int[nums.length];
+        down[0] = 1;
+        up[0] = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up[i] = down[i -1] + 1;
+                down[i] = down[i - 1];
+            } else if (nums[i] < nums[i - 1]) {
+                up[i] = up[i - 1];
+                down[i] = up[i - 1] + 1;
+            } else {
+                up[i] = up[i -1];
+                down[i] = down[i - 1];
+            }
+        }
+        return Math.max(up[nums.length - 1], down[nums.length - 1]);
+    }
+}
