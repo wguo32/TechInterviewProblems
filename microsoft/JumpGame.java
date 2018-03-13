9577405772d398eb072e3dcb635e840437394771
Shanfang <shanfangzhao@gmail.com>
2018-03-13 10:31:52 -0400

    jump game finally solved with gredy method

--- microsoft/JumpGame.java
+++ microsoft/JumpGame.java
@@ -53,9 +53,6 @@ class Solution {
 
 class Solution {
     public boolean canJump(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return true;
-        }
         int[] memo = new int[nums.length];
         memo[nums.length - 1] = 1;
         for (int i = nums.length - 2; i >= 0; i--) {
@@ -68,5 +65,19 @@ class Solution {
             }
         }
         return memo[0] = 1;
+    }
+}
+
+
+// greedy approach
+class Solution {
+    public boolean canJump(int[] nums) {
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos = 0;
     }
 }
