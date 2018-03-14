4b8ab7417388e19e7b5ca3ba012e62b184c9a1c8
Shanfang <shanfangzhao@gmail.com>
2018-03-14 07:24:20 -0400

    combination sum with template solution

--- uber/CombinationSum.java
+++ uber/CombinationSum.java
@@ -1,3 +1,32 @@
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (candidates == null || candidates.length == 0) {
+            return result;
+        }
+        backtrack(candidates, target, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int[] candidates, int remain, int start, List<Integer> list,
+                            List<List<Integer>> result) {
+        if (remain < 0) {
+            return;
+        }
+        if (remain == 0) {
+            result.add(new ArrayList<Integer>(list));
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            list.add(candidates[i]);
+            backtrack(candidates, remain - candidates[i], i, list, result);
+            list.remove(list.size() - 1);
+        }
+    }
+}
+
+// deprecated solution, there is no need to sort array
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
