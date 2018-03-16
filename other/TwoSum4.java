17e1241152fc96053c66ce027eddca4932152696
Shanfang <shanfangzhao@gmail.com>
2018-03-16 14:51:08 -0400

    two sum4 solved with divide and conquer

--- other/TwoSum4.java
+++ other/TwoSum4.java
@@ -7,6 +7,25 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ class Solution {
+     public boolean findTarget(TreeNode root, int k) {
+         Set<Integer> set = new HashSet<>();
+         return find(root, k, set);
+     }
+     private boolean find(TreeNode root, int k, Set<Integer> set) {
+         if (root == null) {
+             return false;
+         }
+         if (set.contains(k - root.val)) {
+             return true;
+         }
+         set.add(root.val);
+         return find(root.left, k, set) || find(root.right, k, set);
+     }
+ }
+
+
+ // traverse the BST
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
         if (root == null) {
