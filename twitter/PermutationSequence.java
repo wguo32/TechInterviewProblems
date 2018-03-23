6fc08cf48dbc2dc0ed1a8eeadfc1707ceb2ca89d
Shanfang <shanfangzhao@gmail.com>
2018-03-23 10:26:50 -0400

    permutation sequence with division and modulo

--- twitter/PermutationSequence.java
+++ twitter/PermutationSequence.java
@@ -1,17 +1,23 @@
 class Solution {
     public String getPermutation(int n, int k) {
-        StringBuilder sb = new StringBuilder();
-        ArrayList<Integer> num = new ArrayList<Integer>();
-        int fact = 1;
-        for (int i = 1; i <= n; i++) {
-            fact *= i;
-            num.add(i);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(i + 1);
         }
-        for (int i = 0, l = k - 1; i < n; i++) {
-            fact /= (n - i);
-            int index = (l / fact);
-            sb.append(num.remove(index));
-            l -= index * fact;
+
+        int[] factorial = new int[n];
+        factorial[0] = 1;
+        for (int i = 1; i < n; i++) {
+            factorial[i] = factorial[i - 1] * i;
+        }
+
+        k = k - 1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = n; i > 0; i--) {
+            int leadingIndex = k / factorial[i - 1];
+            k = k % factorial[i - 1];
+            sb.append(list.get(leadingIndex));
+            list.remove(leadingIndex);
         }
         return sb.toString();
     }
