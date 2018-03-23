7c51c541c88afc3f714ed7bf676c8db2c2e99adf
Shanfang <shanfangzhao@gmail.com>
2018-03-23 11:40:43 -0400

    unique paths2 basic method

---
+++ bloomberg/UniquePaths2.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) {
+            return 0;
+        }
+
+        int[][] track = new int[m][n];
+        track[0][0] = 1;
+        for (int i = 1; i < n; i++) {
+            if (obstacleGrid[0][i] == 1) {
+                break;
+            }
+            track[0][i] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                break;
+            }
+            track[i][0] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    track[i][j] = track[i - 1][j] + track[i][j - 1];
+                }
+            }
+        }
+        return track[m - 1][n - 1];
+    }
+}
