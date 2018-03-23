75939135c54b5f5bafe92defbe8d92e98291b27e
Shanfang <shanfangzhao@gmail.com>
2018-03-23 10:56:29 -0400

    unique paths with better space complexity

--- other/UniquePaths.java
+++ other/UniquePaths.java
@@ -16,3 +16,22 @@ class Solution {
         return matrix[m - 1][n - 1];
     }
 }
+
+// space complexity O(min(m,n)) using two rows to keep path source
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int track[][] = new int[2][n];
+        for (int i = 0; i < n; i++) {
+            track[0][i] = 1;
+        }
+        track[1][0] = 1;
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                track[i % 2][j] = track[i % 2][j - 1] + track[(i - 1) % 2][j];
+            }
+        }
+        return track[(m - 1) % 2][n - 1];
+    }
+}
