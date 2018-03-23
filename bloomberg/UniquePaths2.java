d082effe121461b2ff3a56eee067595d9505c5cd
Shanfang <shanfangzhao@gmail.com>
2018-03-23 12:05:24 -0400

    unique paths with better space complexity

--- bloomberg/UniquePaths2.java
+++ bloomberg/UniquePaths2.java
@@ -32,3 +32,24 @@ class Solution {
         return track[m - 1][n - 1];
     }
 }
+
+
+// with better space complexity
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[] track = new int[n];
+        track[0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    track[j] = 0;
+                } else if (j > 0){
+                    track[j] += track[j - 1];
+                }
+            }
+        }
+        return track[n - 1];
+    }
+}
