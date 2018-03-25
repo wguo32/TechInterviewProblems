a1db007502e7e60cddd0aa4770583406e45743de
Shanfang <shanfangzhao@gmail.com>
2018-03-25 13:48:36 -0400

    house robber with constant space complexity

--- airbnb/HouseRobber.java
+++ airbnb/HouseRobber.java
@@ -12,3 +12,21 @@ class Solution {
         return dp[nums.length];
     }
 }
+
+
+// space complexity O(1)
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int preMax = 0;
+        int curMax = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int temp = curMax;
+            curMax = Math.max(curMax, preMax + nums[i]);
+            preMax = temp;
+        }
+        return curMax;
+    }
+}
