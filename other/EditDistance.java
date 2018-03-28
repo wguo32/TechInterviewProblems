22e918c250d08601874c317503bc103a82cd71a4
Shanfang <shanfangzhao@gmail.com>
2018-03-28 09:19:49 -0400

    refactor edit distance with O(N) space complexity

--- other/EditDistance.java
+++ other/EditDistance.java
@@ -26,3 +26,37 @@ class Solution {
         return dp[len1][len2];
     }
 }
+
+// optimize to O(N) space
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int len1 = word1.length();
+        int len2 = word2.length();
+        if (word1.length() == 0) {
+            return word2.length();
+        }
+        if (word2.length() == 0) {
+            return word1.length();
+        }
+
+        int[][] dp = new int[2][len2 + 1];
+        for (int i = 0; i <= len2; i++) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i <= len1; i++) {
+            dp[i % 2][0] = i;
+            for (int j = 1; j <= len2; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i % 2][j] = dp[(i - 1) % 2][j - 1];
+                } else {
+                    dp[i % 2][j] = Math.min(
+                        dp[(i - 1) % 2][j - 1],
+                        Math.min(dp[(i - 1) % 2][j], dp[i % 2][j - 1])
+                    ) + 1;
+                }
+            }
+        }
+        return dp[len1 % 2][len2];
+    }
+}
