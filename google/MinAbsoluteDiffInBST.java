3f547369e5e94d7ad322b4f6c3aefc312672a5d0
Shanfang <shanfangzhao@gmail.com>
2018-03-29 23:10:39 -0400

    minimum absolute difference in BST

---
+++ google/MinAbsoluteDiffInBST.java
@@ -0,0 +1,20 @@
+class Solution {
+    int min = Integer.MAX_VALUE;
+    Integer prev = null;
+
+    public int getMinimumDifference(TreeNode root) {
+        if (root == null) return min;
+
+        getMinimumDifference(root.left);
+
+        if (prev != null) {
+            min = Math.min(min, root.val - prev);
+        }
+        prev = root.val;
+
+        getMinimumDifference(root.right);
+
+        return min;
+    }
+
+}
