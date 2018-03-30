a52e24a23c071c588723154a5a6a52dff8c56951
Shanfang <shanfangzhao@gmail.com>
2018-03-30 13:28:18 -0400

    unique BST, similar to catalan number

--- other/UniqueBST.java
+++ other/UniqueBST.java
@@ -1,14 +1,13 @@
 class Solution {
     public int numTrees(int n) {
-        int [] G = new int[n+1];
-        G[0] = G[1] = 1;
-
-        for(int i=2; i<=n; ++i) {
-        	for(int j=1; j<=i; ++j) {
-        		G[i] += G[j-1] * G[i-j];
-        	}
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
         }
-
-        return G[n];
+        return dp[n];
     }
 }
