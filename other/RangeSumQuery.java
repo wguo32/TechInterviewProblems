afbb7f50ae49e2090782d4e941a531c0f8821e17
Shanfang <shanfangzhao@gmail.com>
2018-03-31 12:51:38 -0400

    trival presum solves the range sum query problem

---
+++ other/RangeSumQuery.java
@@ -0,0 +1,39 @@
+class NumArray {
+    int[] nums;
+    int[] sums;
+    public NumArray(int[] nums) {
+        this.nums = nums;
+        this.sums = new int[nums.length + 1];
+        sums[0] = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sums[i + 1] = sums[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sums[j + 1] - sums[i];
+    }
+}
+
+
+// optimize it without extra space
+class NumArray {
+    private int[] sums;
+    public NumArray(int[] nums) {
+        this.sums = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            sums[i + 1] = sums[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sums[j + 1] - sums[i];
+    }
+}
+
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
