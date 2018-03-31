4012febef903787d00826304105bef42e61cc7f8
Shanfang <shanfangzhao@gmail.com>
2018-03-30 20:39:48 -0400

    min path sum, typical DP problem

--- other/MinimumPathSum.java
+++ other/MinimumPathSum.java
@@ -3,6 +3,33 @@ class Solution {
         if (grid == null || grid.length == 0 || grid[0].length == 0) {
             return 0;
         }
+        int row = grid.length;
+        int col = grid[0].length;
+        int[][]dp = new int[row][col];
+        dp[0][0] = grid[0][0];
+
+        for (int i = 1; i < col; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for (int j = 1; j < row; j++) {
+            dp[j][0] = dp[j - 1][0] + grid[j][0];
+        }
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[row - 1][col - 1];
+    }
+}
+
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
 
         int m = grid.length;
         int n = grid[0].length;
