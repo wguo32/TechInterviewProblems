b09dd11f1328a6c128fd4d44e76ac43e752433d6
Shanfang <shanfangzhao@gmail.com>
2018-04-02 09:04:11 -0400

    burst ballons with bottom up memoization

--- google/BurstBallons.java
+++ google/BurstBallons.java
@@ -1,4 +1,4 @@
-// use memoization
+// use memoization top down approach
 class Solution {
     public int maxCoins(int[] nums) {
         int len = nums.length;
@@ -27,6 +27,45 @@ class Solution {
                                 nums2[left - 1] * nums2[i] * nums2[right + 1]);
         }
         return memo[left][right];
+    }
+}
+public class Solution {
+    public int maxCoins(int[] nums) {
+        int dp[][] = new int[nums.length][nums.length];
+
+        for (int len = 1; len <= nums.length; len++) { // length of subarray
+            for (int i = 0; i <= nums.length - len; i++) { // start index of subarray
+                int j = i + len - 1;
+                for (int k = i; k <= j; k++) {
+                    //leftValue/rightValue is initially 1. If there is element on
+                    // left/right of k then left/right value will take that value.
+                    int leftValue = 1;
+                    int rightValue = 1;
+                    if (i != 0) {
+                        leftValue = nums[i-1];
+                    }
+                    if (j != nums.length -1) {
+                        rightValue = nums[j+1];
+                    }
+
+                    // left is initially 0. If k is i then left will
+                    //stay 0 otherwise it gets value dp[i][k-1]
+                    // right is similarly 0 initially. if k is j then right will
+                    //stay 0 otherwise it will get value dp[k+1][j]
+                    int left = 0;
+                    int right = 0;
+                    if (i != k) {
+                        left = dp[i][k-1];
+                    }
+                    if (j != k) {
+                        right = dp[k+1][j];
+                    }
+                    dp[i][j] = Math.max(leftValue * nums[k] * rightValue + left + right,
+                            dp[i][j]);
+                }
+            }
+        }
+        return dp[0][nums.length - 1];
     }
 }
 
