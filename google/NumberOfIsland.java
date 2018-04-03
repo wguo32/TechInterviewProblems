4fb0cd70d4ccc2b688433dad0b6532e08eab66bc
Shanfang <shanfangzhao@gmail.com>
2018-04-03 17:43:17 -0400

    number of island with DFS

--- google/NumberOfIsland.java
+++ google/NumberOfIsland.java
@@ -47,3 +47,35 @@ class Solution {
         return num;
     }
 }
+
+// DFS
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int row = grid.length, col = grid[0].length;
+        int num = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == '1') {
+                    num++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return num;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 ||
+            j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+    }
+}
