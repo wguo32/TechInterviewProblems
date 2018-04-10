59cfbd3ce3aebd61a214fb304a3599bd8de96427
Shanfang <shanfangzhao@gmail.com>
2018-04-09 20:47:07 -0400

    use Union-Find to find number of island

--- google/NumberOfIsland.java
+++ google/NumberOfIsland.java
@@ -1,4 +1,4 @@
-// BFS
+// BFS time complexity: O(M* N), space complexity min(M,N)
 class Solution {
     class Position {
         int x;
@@ -48,7 +48,7 @@ class Solution {
     }
 }
 
-// DFS
+// DFS time complexity: O(M* N), space complexity O(M* N)
 class Solution {
     public int numIslands(char[][] grid) {
         if (grid == null || grid.length == 0 || grid[0].length == 0) {
@@ -77,5 +77,81 @@ class Solution {
         dfs(grid, i, j + 1);
         dfs(grid, i + 1, j);
         dfs(grid, i, j - 1);
+    }
+}
+
+
+// Union Find
+
+class Solution {
+    class UnionFind {
+        private int[] parent;
+        private int count;
+        UnionFind(int num) {
+            this.parent = new int[num];
+            for (int i = 0; i < num; i++) {
+                parent[i] = i;
+            }
+        }
+        public void setCount(int num) {
+            this.count = num;
+        }
+        public int getCount() {
+            return count;
+        }
+
+        public int find(int a) {
+            if (parent[a] == a) {
+                return a;
+            }
+            return parent[a] = find(parent[a]);
+        }
+
+        public void union(int a, int b) {
+            int rootA = find(a);
+            int rootB = find(b);
+            if (rootA != rootB) {
+                parent[a] = rootB;
+                count--;
+            }
+        }
+    }
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
+        int row = grid.length;
+        int col = grid[0].length;
+        int count = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                }
+            }
+        }
+        UnionFind uf = new UnionFind(row * col);
+        uf.setCount(count);
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == '1') {
+                    if (i - 1 >= 0 && grid[i - 1][j] == '1') {
+                        uf.union(i * row + j, (i - 1) * row + j);
+                    }
+                    if (j + 1 < col && grid[i][j + 1] == '1') {
+                        uf.union(i * row + j, i * row + j + 1);
+                    }
+                    if (i + 1 < row && grid[i + 1][j] == '1') {
+                        uf.union(i * row + j, (i + 1) * row + j);
+                    }
+                    if (j - 1 >= 0 && grid[i][j - 1] == '1') {
+                        uf.union(i * row + j, i * row  + j - 1);
+                    }
+                }
+            }
+        }
+        return uf.getCount();
     }
 }
