a1f510470ed18b6547399d3dd16f8f7312acdce7
Shanfang <shanfangzhao@gmail.com>
2018-04-16 23:25:23 -0400

    detect cycle in linked list

---
+++ alibaba/LinkedListCycle.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != slow && fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (fast == slow) {
+            return true;
+        }
+        return false;
+    }
+}
