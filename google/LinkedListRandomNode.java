41cb8301efa7f87fbe87fc9ae69556052ba2adc9
Shanfang <shanfangzhao@gmail.com>
2018-05-02 23:34:37 -0400

    play with resivor sampling

---
+++ google/LinkedListRandomNode.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    public Solution(ListNode head) {
+
+    }
+
+    /** Returns a random node's value. */
+    public int getRandom() {
+
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
