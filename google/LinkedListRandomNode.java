ccdf91936e9fb1de38fbf5f6be3f4456d00de92a
Shanfang <shanfangzhao@gmail.com>
2018-05-04 21:59:34 -0400

    referenced discussion for random node in linked list

--- google/LinkedListRandomNode.java
+++ google/LinkedListRandomNode.java
@@ -7,16 +7,29 @@
  * }
  */
 class Solution {
+    ListNode head;
+    Random random;
 
     /** @param head The linked list's head.
         Note that the head is guaranteed to be not null, so it contains at least one node. */
-    public Solution(ListNode head) {
+        public Solution(ListNode h) {
+            head = h;
+            random = new Random();
+        }
 
-    }
 
     /** Returns a random node's value. */
     public int getRandom() {
 
+        ListNode c = head;
+        int r = c.val;
+        for(int i=1;c.next != null;i++){
+
+            c = c.next;
+            if(random.nextInt(i + 1) == i) r = c.val;
+        }
+
+        return r;
     }
 }
 
