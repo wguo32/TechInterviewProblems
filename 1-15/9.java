c1357de0587905c6c3d416bf4e6e4cbd556feaa1
Shanfang <shanfangzhao@gmail.com>
2018-05-07 18:20:47 -0400

    palindrome number

---
+++ 1-15/9.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || x != 0 && x % 10 == 0) {
+            return false;
+        }
+        int reverse = 0;
+        while (x > reverse) {
+            reverse = reverse * 10 + x % 10;
+            x = x / 10;
+        }
+        return (x == reverse || x == reverse / 10);
+    }
+}
