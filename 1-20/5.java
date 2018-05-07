96c0ea321b57d38c6fdf63cfbe99f261cbd4632f
Shanfang <shanfangzhao@gmail.com>
2018-05-07 15:52:26 -0400

    find longest palindromic substring by expanding center

---
+++ 1-20/5.java
@@ -0,0 +1,29 @@
+// expand center
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+
+        int start = 0, end = 0;
+        int maxLen = 1;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start + 1) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expand(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
