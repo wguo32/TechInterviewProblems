d3479a630d66218f6e36baea1103844b7603f740
Shanfang <shanfangzhao@gmail.com>
2018-05-07 16:43:03 -0400

    find longest palindromic substring using DP

--- 1-20/5.java
+++ 1-20/5.java
@@ -27,3 +27,30 @@ class Solution {
         return right - left - 1;
     }
 }
+
+// DP
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+        int maxLen = 0;
+        String result = "";
+        for (int i = len - 1; i >= 0; i--) {
+            for (int j = i; j < len; j++) {
+                if (s.charAt(i) == s.charAt(j) && (i + 1 >= j - 1 || dp[i + 1][j - 1])) {
+                    dp[i][j] = true;
+                    if (j - i + 1 > maxLen) {
+                        maxLen = j - i + 1;
+                        result = s.substring(i, j + 1);
+                    }
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return result;
+    }
+}
