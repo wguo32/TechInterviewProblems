72ceb1707f2b5ac8cfe33692332fd903b9707a10
Shanfang <shanfangzhao@gmail.com>
2018-05-08 17:35:25 -0400

    fix logic error in three sum closest

--- bloomberg/ThreeSumClosest.java
+++ bloomberg/ThreeSumClosest.java
@@ -6,7 +6,7 @@ class Solution {
         Arrays.sort(nums);
         int closeSum = nums[0] + nums[1] + nums[nums.length - 1];
         for (int i = 0; i < nums.length - 2; i++) {
-            if (i ==0 && nums[i - 1] != nums[i]) { // to skip duplicates
+            if (i ==0 || nums[i - 1] != nums[i]) { // to skip duplicates
                 int start = i + 1, end = nums.length - 1;
                 while (start < end) {
                     int sum = nums[start] + nums[end] + nums[i];
