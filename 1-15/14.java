848129176abebd2421a5741b15ea8d56d05934c8
Shanfang <shanfangzhao@gmail.com>
2018-05-08 14:54:31 -0400

    longest common prefix with vertical scanning

--- 1-15/14.java
+++ 1-15/14.java
@@ -17,3 +17,22 @@ class Solution {
         return prefix;
     }
 }
+
+// vertical scanning
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j].length() == i || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
