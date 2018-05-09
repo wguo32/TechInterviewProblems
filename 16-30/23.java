3ff145e0857eec09b050d704d6debd1e1e8f6eb1
Shanfang <shanfangzhao@gmail.com>
2018-05-09 10:41:06 -0400

    merge K sorted lists with divide and conquer

--- 16-30/23.java
+++ 16-30/23.java
@@ -38,3 +38,49 @@ class Solution {
         return dummy.next;
     }
 }
+
+
+// use divide and conquer
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        return helper(lists, 0, lists.length - 1);
+    }
+
+    private ListNode helper(ListNode[] lists, int start, int end) {
+        if (start >= end) {
+            return lists[start];
+        }
+        int mid = start + (end - start) / 2;
+        ListNode first = helper(lists, start, mid);
+        ListNode second = helper(lists, mid + 1, end);
+        return mergeTwoLists(first, second);
+    }
+
+    private ListNode mergeTwoLists(ListNode head1, ListNode head2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+
+        while (head1 != null && head2 != null) {
+            if (head1.val < head2.val) {
+                cur.next = head1;
+                head1 = head1.next;
+            } else {
+                cur.next = head2;
+                head2 = head2.next;
+            }
+            cur = cur.next;
+        }
+
+        if (head1 != null) {
+            cur.next = head1;
+        }
+        if (head2 != null) {
+            cur.next = head2;
+        }
+        return dummy.next;
+    }
+}
