1f5819deaf77abc52134e40b45fdcbe18e22034a
Shanfang <shanfangzhao@gmail.com>
2018-05-10 16:13:50 -0400

    valid sudoku with three hash sets

---
+++ 31-40/36.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            Set<Character> rows = new HashSet<>();
+            Set<Character> cols = new HashSet<>();
+            Set<Character> cubes = new HashSet<>();
+
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] != '.' && !rows.add(board[i][j])) {
+                    return false;
+                }
+                if (board[j][i] != '.' && !cols.add(board[j][i])) {
+                    return false;
+                }
+
+                int rowIndex = 3 * (i / 3);
+                int colIndex = 3 * (i % 3);
+
+                if (board[rowIndex + j / 3][colIndex + j % 3] != '.' &&
+                    !cubes.add(board[rowIndex + j / 3][colIndex + j % 3])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
