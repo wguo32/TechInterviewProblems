89bf4482e2aae45cdb900938920d4a7d46565ab3
Shanfang <shanfangzhao@gmail.com>
2018-05-13 08:44:50 -0400

    another way to handle duplicates for permutatios

--- 41-50/47.java
+++ 41-50/47.java
@@ -1,3 +1,43 @@
+// another way to handle duplicates
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+
+        Arrays.sort(nums);
+        boolean[] used = new boolean[nums.length];
+        helper(nums, used, new ArrayList<Integer>(), result);
+        return result;
+    }
+
+    private void helper(int[] nums, boolean[] used, ArrayList<Integer> list,
+                        List<List<Integer>> result) {
+        if (list.size() == nums.length) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+
+            used[i] = true;
+            list.add(nums[i]);
+            helper(nums, used, list, result);
+            used[i] = false;
+            list.remove(list.size() - 1);
+
+            while (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+                i++;
+            }
+
+        }
+    }
+}
+
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
