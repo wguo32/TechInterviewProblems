a3d793dee6073243df8a4de698a7225f27661bf4
Shanfang <shanfangzhao@gmail.com>
2018-05-17 21:43:31 -0400

    rotate image using some tricks

--- 41-50/48.java
+++ 41-50/48.java
@@ -24,3 +24,32 @@ class Solution {
         }
     }
 }
+
+// transpose and flip
+class Solution {
+    public void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return;
+        }
+
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int start = 0, end = matrix.length - 1;
+            while (start < end) {
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
