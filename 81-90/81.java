9ffc0dde1ab9b5017e6c9b42f624f3d33a96edcd
Shanfang <shanfangzhao@gmail.com>
2018-05-30 21:37:56 -0400

    search in rotated sorted array with issue

---
+++ 81-90/81.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] >= nums[start]]) {
+                if (nums[mid] >= target && target >= nums[start]) {
+                    end = mid;
+                } else {
+                    start = mid;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[end]) {
+                    start = mid;
+                } else {
+                    end = mid;
+                }
+            }
+        }
+
+        if (nums[start] == target || nums[end] == target) {
+            return true;
+        }
+        return false
+    }
+}
