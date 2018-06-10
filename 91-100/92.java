c22453ec8b5f84ca08be69045247c092ddf4315c
Shanfang <shanfangzhao@gmail.com>
2018-06-09 22:56:17 -0400

    reverse linked list

---
+++ 91-100/92.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (head == null || m == n) {
+            return head;
+        }
+        int count = 1;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        head = dummy;
+        while(count < m) {
+            head = head.next;
+            count++;
+        }
+
+        ListNode preM = head;
+        ListNode mNode = head.next;
+        ListNode nNode = mNode;
+        ListNode postN = nNode.next;
+
+        while (count < n) {
+            ListNode temp = postN.next;
+            postN.next = nNode;
+            nNode = postN;
+            postN = temp;
+            count++;
+        }
+        preM.next = nNode;
+        mNode.next = postN;
+        return dummy.next;
+    }
+}
