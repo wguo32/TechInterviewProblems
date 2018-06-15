0ee75cdd68f0a6fcc557108152e58da628ad7bbe
Shanfang <shanfangzhao@gmail.com>
2018-06-14 20:55:58 -0400

    unique BST2 with recursion

---
+++ 91-100/95.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n <= 0) {
+            return new ArrayList<TreeNode>();
+        }
+        return helper(1, n);
+    }
+
+    private List<TreeNode> helper(int start, int end) {
+        List<TreeNode> result = new ArrayList<TreeNode>();
+        if (start > end) {
+            result.add(null);
+            return result;
+        }
+
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftChildren = helper(start, i - 1);
+            List<TreeNode> rightChildren = helper(i + 1, end);
+            for (TreeNode l : leftChildren) {
+                for (TreeNode r : rightChildren) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    result.add(root);
+                }
+            }
+        }
+        return result;
+    }
+}
