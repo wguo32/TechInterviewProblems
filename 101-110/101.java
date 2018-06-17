4bb87b760007d4e4fb265139b199fbe7a53790d7
Shanfang <shanfangzhao@gmail.com>
2018-06-17 15:19:23 -0400

    check symmetric tree iteratively

--- 101-110/101.java
+++ 101-110/101.java
@@ -7,28 +7,31 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            TreeNode p = queue.poll();
-            TreeNode q = queue.poll();
-            if (p == null && q == null) {
-                continue;
-            }
-            if (p == null || q == null) {
-                return false;
-            }
-            if (p.val != q.val) {
-                return false;
-            }
-            queue.offer(p.left);
-            queue.offer(q.right);
-            queue.offer(p.right);
-            queue.offer(q.left);
-        }
-        return true;
-    }
-}
+ class Solution {
+     public boolean isSymmetric(TreeNode root) {
+         if (root == null || (root.left == null && root.right == null)) {
+             return true;
+         }
+         if (root.left == null || root.right == null) {
+             return false;
+         }
+         Queue<TreeNode> queue = new LinkedList<>();
+         queue.offer(root.left);
+         queue.offer(root.right);
+         while (!queue.isEmpty()) {
+             TreeNode node1 = queue.poll();
+             TreeNode node2 = queue.poll();
+             if (node1 == null && node2 == null) {
+                 break;
+             }
+             if (node1 == null || node2 == null || (node1.val != node2.val)) {
+                 return false;
+             }
+             queue.offer(node1.left);
+             queue.offer(node2.right);
+             queue.offer(node1.right);
+             queue.offer(node2.left);
+         }
+         return true;
+     }
+ }
--- Azure/SymmetricTree.java
+++ Azure/SymmetricTree.java
@@ -25,3 +25,33 @@ class Solution {
                     && checkSym(node1.right, node2.left);
     }
 }
+
+// iterative approach
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return true;
+        }
+        if (root.left == null || root.right == null) {
+            return false;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root.left);
+        queue.offer(root.right);
+        while (!queue.isEmpty()) {
+            TreeNode node1 = queue.poll();
+            TreeNode node2 = queue.poll();
+            if (node1 == null && node2 == null) {
+                break;
+            }
+            if (node1 == null || node2 == null || (node1.val != node2.val)) {
+                return false;
+            }
+            queue.offer(node1.left);
+            queue.offer(node2.right);
+            queue.offer(node1.right);
+            queue.offer(node2.left);
+        }
+        return true;
+    }
+}
