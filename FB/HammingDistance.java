73709bc2c3ce626d1055764e90b5394c18153be5
Shanfang <shanfangzhao@gmail.com>
2018-06-18 19:47:19 -0400

    hamming distance with OXR

--- FB/HammingDistance.java
+++ FB/HammingDistance.java
@@ -13,3 +13,16 @@ class Solution {
         return dist;
     }
 }
+
+// with OXR
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int num = x ^ y;
+        int dist = 0;
+        while (num > 0) {
+            dist += num & 1;
+            num >>= 1;
+        }
+        return dist;
+    }
+}
