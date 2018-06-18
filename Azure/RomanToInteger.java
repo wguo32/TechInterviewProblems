dfb47b5ce250f834d8c39e281f8f72562565aab4
Shanfang <shanfangzhao@gmail.com>
2018-06-17 21:59:16 -0400

    no hashmap used to convert roman to integer

--- Azure/RomanToInteger.java
+++ Azure/RomanToInteger.java
@@ -27,3 +27,37 @@ class Solution {
         return sum;
     }
 }
+
+// no hashmap
+class Solution {
+    public int romanToInt(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int pre = getNum(s.charAt(0));
+        int sum = pre;
+        for (int i = 1; i < s.length(); i++) {
+            int cur = getNum(s.charAt(i));
+            sum += cur;
+            if (cur > pre) {
+                sum -= 2 * pre;
+            }
+            pre = cur;
+        }
+        return sum;
+    }
+
+    private int getNum(char c) {
+        switch (c) {
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+        }
+        return 0;
+    }
+}
