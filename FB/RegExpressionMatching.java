7c93e2f001ba4df9e547031e05a1e8e679d60203
Shanfang <shanfangzhao@gmail.com>
2018-06-26 22:16:24 -0400

    regular expression matching with DP

--- FB/RegExpressionMatching.java
+++ FB/RegExpressionMatching.java
@@ -12,3 +12,34 @@ class Solution {
         }
     }
 }
+
+
+// use DP
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= p.length(); i++) {
+            if (p.charAt(i - 1) == '*') {
+                dp[0][i] = dp[0][i - 2];
+            }
+        }
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= p.length(); j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    if (p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.') {
+                       dp[i][j] = dp[i][j - 2] || dp[i - 1][j - 1] || dp[i - 1][j];
+                    } else {
+                        dp[i][j] = dp[i][j - 2];
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+}
