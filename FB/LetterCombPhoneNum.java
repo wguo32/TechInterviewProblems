4d15a66df4e94c0be4de72b972dac74378afd1a2
Shanfang <shanfangzhao@gmail.com>
2018-06-27 22:44:15 -0400

    recursive approach to find letter combination for phone number

--- FB/LetterCombPhoneNum.java
+++ FB/LetterCombPhoneNum.java
@@ -1,3 +1,4 @@
+// recursive approach
 class Solution {
     public List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList<>();
@@ -23,5 +24,29 @@ class Solution {
             helper(digits, start + 1, map, result, sb);
             sb.deleteCharAt(sb.length() - 1);
         }
+    }
+}
+
+// iterative approach
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if (digits == null || digits.length() == 0) {
+            return new ArrayList<>();
+        }
+
+        String[] map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        LinkedList<String> list = new LinkedList<>();
+        list.offer("");
+
+        while (!list.isEmpty()) {
+            if (list.peek().length() == digits.length()) {
+                break;
+            }
+            String str = list.poll();
+            for (char c : map[digits.charAt(str.length()) - '0'].toCharArray()) {
+                list.offer(str + c);
+            }
+        }
+        return list;
     }
 }
