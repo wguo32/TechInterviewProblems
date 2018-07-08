4251936273707d069d119bdc736ed7a97b21702b
Shanfang <shanfangzhao@gmail.com>
2018-07-08 19:43:51 -0400

    implement strStr with two for loops

--- FB/ImplementStrStr.java
+++ FB/ImplementStrStr.java
@@ -11,3 +11,25 @@ class Solution {
         return -1;
     }
 }
+
+// use two for loops
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null) {
+            return -1;
+        }
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            int j = 0;
+            for (j = 0; j < needle.length(); j++) {
+                if (needle.charAt(j) != haystack.charAt(i + j)) {
+                    break;
+                }
+            }
+            if (j == needle.length()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
