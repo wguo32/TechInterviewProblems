23bc96f1ae7eda622f17f55e84a21fd93cbad0f9
Shanfang <shanfangzhao@gmail.com>
2018-07-09 21:08:34 -0400

    use hashset to find missing number

--- FB/MissingNumber.java
+++ FB/MissingNumber.java
@@ -17,3 +17,20 @@ class Solution {
         return -1;
     }
 }
+
+// hashset with linear time complexity
+class Solution {
+    public int missingNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        for (int i = 0; i < nums.length + 1; i++) {
+            if (!set.contains(i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
